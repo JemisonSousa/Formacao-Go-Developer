f15cec38514c314199f31d715b59d4e1444500ab
Jemison <56642164+JemisonSousa@users.noreply.github.com>
2024-07-15 23:28:30 +0000

    2

---
+++ pacotes/string.go
@@ -0,0 +1,14 @@
+//pacotes (caixinhas de funções)
+//função contains
+//Contains: procurar dentro de uma string outra string menor
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Contains("Computador", "com"))
+}
